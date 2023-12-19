e630fdefca38b6ecac1f75841eb280e88d226e14
Michael Widegren <116347494+cybertron-payex@users.noreply.github.com>
2023-12-19 11:15:20 +0100

    Update entrypoint.sh
    [skip ci]

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
-echo "Greetings $1 might be the best in the world" >> "$GITHUB_OUTPUT"
+echo "Greetings $1 might be the best in the world" 
+echo "time=$(date)" >> $GITHUB_OUTPUT
 exit 0
